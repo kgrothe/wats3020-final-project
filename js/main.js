28d66db18954705f159f1e915a13396a47f1319a
kgrothe <kboyle@seattleu.edu>
2018-06-07 02:41:13 +0000

    First commit - main.css, main.js, index.html.

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// select all buttons with class toggle, add to array
+
+var buttons = document.querySelectorAll(".toggle");
+
+var buttonsCount = buttons.length;
+
+// select all panels with class info
+
+var panels = document.getElementsByClassName("info");
+
+var panelsCount = panels.length;
+
+var a;
+
+// set initial display style of info panels to none
+
+for (a = 0; a < panelsCount; a++) {
+    
+    panels[a].style.display = "none";
+}
+
+var i;
+
+// add event listener to all toggle buttons 
+
+for (i = 0; i < buttonsCount; i++) {
+   
+  buttons[i].addEventListener("click", function() {
+
+// target info panel  
+  
+  var info = this.nextElementSibling; 
+  
+// switch panel style display 
+    
+    if (info.style.display === "none") {
+    info.style.display = "block";
+    
+       } else {
+        
+        info.style.display = "none";
+    }
+       
+  });
+   
+ }   
+
+// add event listener to contact buttons
+
+var contacts = document.querySelectorAll(".contact");
+
+var contactsCount = contacts.length;
+
+var z;
+
+// add event listener to all contact icon buttons
+
+for (z = 0; z < contactsCount; z++){
+
+contacts[z].addEventListener("mouseover", showContact);
+
+}
+
+function showContact () {
+    alert('mouseover!');
+}
+
+//);
+//}
+
