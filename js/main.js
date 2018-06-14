16aeb091e0c3bd63218ea7b8a888fc784aa94aaa
kgrothe <kboyle@seattleu.edu>
2018-06-14 04:20:53 +0000

    Revised main.css, main.js, index.html.

--- js/main.js
+++ js/main.js
@@ -18,11 +18,11 @@ let contactsCount = contacts.length;
 let bios = document.getElementsByClassName("bio");
 let biosCount = bios.length;
 
-// initialize summary 
+// initialize summary
 
-var summary = [];
-var summaryList = [];
-var summaryAll = [];
+let summary = [];
+let summaryList = [];
+let summaryAll = [];
 
 let popup = document.getElementsByClassName("popup");
 
@@ -32,68 +32,63 @@ var i;
 
 // set initial display style of info panels to none
 
-for (i = 0; i < panelsCount; i++) {    
+for (i = 0; i < panelsCount; i++) {
   panels[i].style.display = "none";
 }
 
 // create summary of information in bio ul (contact info)
 
 for (i = 0; i < biosCount; i++) {
-  summary = bios[i].innerHTML;    
+  summary = bios[i].innerHTML;
         
     // add to an array
     summaryList = summary.split(",");
         
-    summaryAll.push(summaryList);     
+    summaryAll.push(summaryList); 
     
 }
     
 // set initial display style of bio cards to none
 
-for (i = 0; i < biosCount; i++) {      
-    
+for (i = 0; i < biosCount; i++) {
   bios[i].style.display = "none";  
-}  
+}
 
-// add event listener to all toggle buttons 
+// add event listener to all toggle buttons
 
 for (i = 0; i < buttonsCount; i++) {
-   
   buttons[i].addEventListener("click", function() {
 
-// target info panel  
+// target info panel
   
-  var info = this.nextElementSibling; 
+  let info = this.nextElementSibling;
   
-// switch panel style display 
+// switch panel style display
     
     if (info.style.display === "none") {
       info.style.display = "block";
     
     } else {
-        
       info.style.display = "none";
-    }
-       
-  });
-   
- }   
+    }       
+  }); 
+}   
 
 // add event listener to all contact icon buttons to show contact info
 
 for (i = 0; i < contactsCount; i++){
 
-  contacts[i].addEventListener("click", function() { 
+  contacts[i].addEventListener("click", function() {
 
  // target bio ul
   
-  var bio = this.nextElementSibling;
+  let bio = this.nextElementSibling;
       
   // switch contact bio display
   
     if (bio.style.display === "none") {
-      bio.style.display = "block";        
-    }    
+      bio.style.display = "block";
+    }
   });
 }
 
@@ -101,20 +96,18 @@ for (i = 0; i < contactsCount; i++){
 
 for (i = 0; i < contactsCount; i++){
 
-  contacts[i].addEventListener("dblclick", function() { 
+  contacts[i].addEventListener("dblclick", function() {
 
   // target bio ul
     
-  var bio = this.nextElementSibling;
+  let bio = this.nextElementSibling;
   
   // switch contact bio display
   
     if (bio.style.display === "block"){
-      bio.style.display = "none";  
-    }
-    
-  });
-  
+      bio.style.display = "none";
+    }    
+  });  
 }
 
 // add event listener to summary button to display/hide summary list
@@ -124,13 +117,10 @@ document.getElementById("summary").addEventListener("click", function(){
     if (popup[0].style.display === "none"){
     
       popup[0].style.display = "block";
-      popup[0].innerHTML = `${summaryAll}`;  
+      popup[0].innerHTML = `${summaryAll}`;
     
       } else {
       
         popup[0].style.display = "none";
       }
-        
 });
-
-
